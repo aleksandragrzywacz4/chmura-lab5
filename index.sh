de299a7d35a7b8140421ffa864d77a68d52ac396
aleksandragrzywacz4 <ola.grzywacz4@gmail.com>
2026-04-20 16:56:22 +0200

    Delete index.sh

--- index.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "<html><body>" > index.html
-echo "<p><b>IP serwera:</b> $(hostname -i)</p>" >> index.html
-echo "<p><b>Nazwa serwera (hostname):</b> $(hostname)</p>" >> index.html
-echo "<p><b>Wersja aplikacji:</b> $APP_VERSION</p>" >> index.html
-echo "</body></html>" >> index.html
